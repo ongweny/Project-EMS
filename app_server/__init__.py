abe7bb8103c9bfb5fc8a659165aa94ef5da037fa
ongweny <ongwenyjoel@gmail.com>
2024-08-13 11:46:37 +0300

    bug fixing

---
+++ app_server/__init__.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+from flask_cors import CORS
+from app_server.config import Config
+
+db = SQLAlchemy()
+migrate = Migrate()
+bcrypt = Bcrypt()
+jwt = JWTManager()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    bcrypt.init_app(app)
+    jwt.init_app(app)
+    CORS(app)
+
+    with app.app_context():
+        from app_server.models import User, Event, Reservation  
+        db.create_all()  
+    return app
---
+++ app_server/config.py
@@ -0,0 +1,31 @@
+# import os
+
+# class Config:
+#     SECRET_KEY = os.environ.get('SECRET_KEY', 'your_secret_key')
+#     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///app.db')
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
+#     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'your_jwt_secret_key')
+
+import logging
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+
+    base_dir = os.path.abspath(os.path.dirname(__file__))
+    db_path = os.path.join(base_dir, 'instance', 'app.db')
+
+    os.makedirs(os.path.dirname(db_path), exist_ok=True)
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or f'sqlite:///{db_path}'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'your_jwt_secret_key'
+    LOG_LEVEL = logging.DEBUG
+
+    @staticmethod
+    def init_app(app):
+        handler = logging.StreamHandler()
+        handler.setLevel(Config.LOG_LEVEL)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        app.logger.addHandler(handler)
